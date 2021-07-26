b6bbfd79eb7f1cf7582f00dbfab61300a9e7ea59
peterdotdev <peterdotdev@gmail.com>
2021-07-26 19:15:28 -0300

    fixing mobile bug20

--- day01-drumkit/script.js
+++ day01-drumkit/script.js
@@ -11,14 +11,6 @@ window.addEventListener('keydown', function(e) {
 window.onload = function() {
     let iterable = [65,83,68,70,71,72,74,75,76];
     for (let value of iterable) {
-        document.querySelector(`.key[data-key="${value}"]`).addEventListener('click', function() {
-            const audio = document.querySelector(`audio[data-key="${value}"]`);
-            if (!audio) return;
-            audio.currentTime = 0;
-            audio.play();
-            document.querySelector(`.key[data-key="${value}"]`).classList.add("playing");
-            setTimeout(function() { document.querySelector(`.key[data-key="${value}"]`).classList.remove("playing"); }, 100);
-        });
         document.querySelector(`.key[data-key="${value}"]`).addEventListener('touchstart', function() {
             const audio = document.querySelector(`audio[data-key="${value}"]`);
             if (!audio) return;
